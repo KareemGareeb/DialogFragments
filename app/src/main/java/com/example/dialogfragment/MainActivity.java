6abac318dc48634b900e1f7d346bd831fe5c8ee4
kareem <gharib@hists.edu.ly.com>
2020-09-21 22:34:03 +0200

    codePath example

--- app/src/main/java/com/example/dialogfragment/MainActivity.java
+++ app/src/main/java/com/example/dialogfragment/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.dialogfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // new MyDialog().show(getSupportFragmentManager(), "MyDialog");
+        showAlertDialog();
+    }
+
+    private void showAlertDialog() {
+        FragmentManager fm = getSupportFragmentManager();
+        MyDialog alertDialog = MyDialog.newInstance("Some title");
+        alertDialog.show(fm, "fragment_alert");
     }
 }
