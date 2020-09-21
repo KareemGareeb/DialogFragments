cb22331059010a70afd32147e890dea13558b854
kareem <gharib@hists.edu.ly.com>
2020-09-21 21:59:01 +0200

    codePath example

--- app/src/main/java/com/example/dialogfragment/MyDialog.java
+++ app/src/main/java/com/example/dialogfragment/MyDialog.java
@@ -10,27 +10,40 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class MyDialog extends DialogFragment {
-    Context mContext;
     public MyDialog() {
-        //mContext = getActivity();
+        // Empty constructor required for DialogFragment
     }
+
+    public static MyDialog newInstance(String title) {
+        MyDialog frag = new MyDialog();
+        Bundle args = new Bundle();
+        args.putString("title", title);
+        frag.setArguments(args);
+        return frag;
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        String title = getArguments().getString("title");
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
-        alertDialogBuilder.setTitle("Really?");
+        alertDialogBuilder.setTitle(title);
         alertDialogBuilder.setMessage("Are you sure?");
-        //null should be your on click listener
-        alertDialogBuilder.setPositiveButton("OK", null);
-        alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-
+        alertDialogBuilder.setPositiveButton("OK",  new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
+                // on success
             }
         });
+        alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (dialog != null && ((Dialog) dialog).isShowing()) {
+                    dialog.dismiss();
+                }
+            }
 
+        });
 
         return alertDialogBuilder.create();
     }
-
    }
