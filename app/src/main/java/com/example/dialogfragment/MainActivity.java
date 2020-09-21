5ee7a9c36eeb1f4808c495dd368ce456b02e64dc
kareem <gharib@hists.edu.ly.com>
2020-09-21 21:23:05 +0200

    commenting  new dialogFragment for testing

--- app/src/main/java/com/example/dialogfragment/MainActivity.java
+++ app/src/main/java/com/example/dialogfragment/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new MyDialog().show(getSupportFragmentManager(), "MyDialog");
+       // new MyDialog().show(getSupportFragmentManager(), "MyDialog");
     }
 }
